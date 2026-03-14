5d56c083ed866f7b1ee8e16f9630408fddfe2754
RichardFuentes369 <javierbaron6@gmail.com>
2026-03-13 21:36:56 -0500

    ajustes

---
+++ angular/src/app/mods/catalog/admin/pages/proveedores/components/reporte/reporte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedores-reporte',
+  standalone: true,
+  templateUrl: './reporte.component.html',
+  styleUrl: './reporte.component.scss',
+})
+export class ReporteComponent {
+
+}
