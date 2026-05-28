06baf2783d9c1f030cf2c6596b4d06e60667e3b4
RichardFuentes369 <javierbaron6@gmail.com>
2026-05-27 20:03:07 -0500

    ajustes

--- angular/src/app/mods/main/admin/pages/menu/menu.component.ts
+++ angular/src/app/mods/main/admin/pages/menu/menu.component.ts
@@ -20,7 +20,6 @@ import {
   LAYOUT_ADMIN_PAGE_LOSS, 
   LAYOUT_ADMIN_PAGE_WAREHOUSE,
 } from '@mod/main/const/main.const';
-import { CartComponent } from '@component/animation/cart/cart.component';
 
 @Component({
   selector: 'app-mod-menu-admin',
