c65344f943aba6797011e57a99aced7df15f0ce6
RichardFuentes369 <javierbaron6@gmail.com>
2026-07-09 19:49:05 -0500

    ajustes

---
+++ angular/src/app/mods/main/index/components/contactanos/contactanos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-component-contactanos',
+  standalone: true,
+  templateUrl: './contactanos.component.html',
+  styleUrl: './contactanos.component.scss',
+})
+export class ContactanosComponent {
+
+}
