926b68b1f1aca8f27884902e6c474c902f17a35a
RichardFuentes369 <javierbaron6@gmail.com>
2026-07-20 20:31:01 -0500

    ajustes

--- angular/src/app/components/globales/notification/notification.component.ts
+++ angular/src/app/components/globales/notification/notification.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-globales-notification',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss',
 })
 export class NotificationComponent {
+
+  isDarkMode: string = ''
+
+  @HostListener('window:themeChanged', ['$event'])
+  onThemeChanged(event: Event): void {
+    const customEvent = event as CustomEvent;
+    this.isDarkMode = customEvent.detail;
+  }
 
 }
